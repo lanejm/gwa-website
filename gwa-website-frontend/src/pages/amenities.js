842523de204fb93aa80c0dbe254baecb98e66430
lanejm <67156874+lanejm@users.noreply.github.com>
2021-04-20 18:57:54 -0400

    amenities page updated with all amenities listed

--- gwa-website-frontend/src/pages/amenities.js
+++ gwa-website-frontend/src/pages/amenities.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faTools,
@@ -9,12 +8,10 @@ import {
   faSoap,
   faLock,
   faUtensils,
-  faGlassCheers,
   faTv,
   faTree,
   faDoorClosed,
   faBed,
-  faBuilding,
   faWalking,
   faBoxOpen,
 } from "@fortawesome/free-solid-svg-icons";
@@ -25,11 +22,12 @@ const Amenities = () => {
     <>
       <section class="hero is-info">
         <div class="hero-body">
-          <h1 class="title">Building Amenities</h1>
+          <h1 class="title">Property Amenities</h1>
         </div>
       </section>
+      <br />
       <div class="columns">
-        <div class="column is-half">
+        <div class="column">
           <div class="card">
             <div class="card-image">
               <FontAwesomeIcon icon={faTools} size="3x" />
@@ -38,12 +36,140 @@ const Amenities = () => {
               <div class="content">On-Site Maintenance</div>
             </div>
           </div>
+          <br />
           <div class="card">
             <div class="card-image">
               <FontAwesomeIcon icon={faLeaf} size="3x" />
             </div>
             <div class="card-content">
               <div class="content">Adjacent to 62 Acre Broad Ripple Park</div>
+            </div>
+          </div>
+          <br />
+          <div class="card">
+            <div class="card-image">
+              <FontAwesomeIcon icon={faWarehouse} size="3x" />
+            </div>
+            <div class="card-content">
+              <div class="content">Covered, secure garage parking</div>
+            </div>
+          </div>
+          <br />
+          <div class="card">
+            <div class="card-image">
+              <FontAwesomeIcon icon={faRecycle} size="3x" />
+            </div>
+            <div class="card-content">
+              <div class="content">On-site recycling</div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div class="column">
+          <div class="card">
+            <div class="card-image">
+              <FontAwesomeIcon icon={faSoap} size="3x" />
+            </div>
+            <div class="card-content">
+              <div class="content">Laundry Facilities</div>
+            </div>
+          </div>
+          <br />
+          <div class="card">
+            <div class="card-image">
+              <FontAwesomeIcon icon={faLock} size="3x" />
+            </div>
+            <div class="card-content">
+              <div class="content">Secure access controlled entrance</div>
+            </div>
+          </div>
+          <br />
+          <div class="card">
+            <div class="card-image">
+              <FontAwesomeIcon icon={faUtensils} size="3x" />
+            </div>
+            <div class="card-content">
+              <div class="content">
+                Delicious Mexican food smells from nearby restaurant--yum!
+              </div>
+            </div>
+          </div>
+          <br />
+          <div class="card">
+            <div class="card-image">
+              <FontAwesomeIcon icon={faWalking} size="3x" />
+            </div>
+            <div class="card-content">
+              <div class="content">
+                Walking distance to several bars and restaurants!
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section class="hero is-info">
+        <div class="hero-body">
+          <h1 class="title">Unit Amenities</h1>
+        </div>
+      </section>
+      <br />
+      <div class="columns">
+        <div class="column">
+          <div class="card">
+            <div class="card-image">
+              <FontAwesomeIcon icon={faBed} size="3x" />
+            </div>
+            <div class="card-content">
+              <div class="content">Carpeted Bedrooms</div>
+            </div>
+          </div>
+          <br />
+          <div class="card">
+            <div class="card-image">
+              <FontAwesomeIcon icon={faTv} size="3x" />
+            </div>
+            <div class="card-content">
+              <div class="content">Basic Spectrum Cable Included</div>
+            </div>
+          </div>
+          <br />
+          <div class="card">
+            <div class="card-image">
+              <FontAwesomeIcon icon={faDoorClosed} size="3x" />
+            </div>
+            <div class="card-content">
+              <div class="content">Large bedroom closets</div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div class="column">
+          <div class="card">
+            <div class="card-image">
+              <FontAwesomeIcon icon={faLock} size="3x" />
+            </div>
+            <div class="card-content">
+              <div class="content">Keypad Deadbolt Locks</div>
+            </div>
+          </div>
+          <br />
+          <div class="card">
+            <div class="card-image">
+              <FontAwesomeIcon icon={faBoxOpen} size="3x" />
+            </div>
+            <div class="card-content">
+              <div class="content">
+                Storage space off of private exterior balcony/terrace
+              </div>
+            </div>
+          </div>
+          <br />
+          <div class="card">
+            <div class="card-image">
+              <FontAwesomeIcon icon={faTree} size="3x" />
+            </div>
+            <div class="card-content">
+              <div class="content">Beautiful Forest Views</div>
             </div>
           </div>
         </div>
@@ -55,32 +181,3 @@ const Amenities = () => {
 export default Amenities;
 
 //style amenities page
-
-{
-  /* <ul>
-<li>On-site maintenance</li>
-<li>Adjacent to 62 Acre Broad Ripple Park</li>
-<li>Covered, secure garage parking</li>
-<li>Located on dead end street, very peaceful!</li>
-<li>On-site recycling</li>
-<li>Laundry Facilities</li>
-<li>Secure Access Controlled Building</li>
-<li>
-  Delicious Mexican food smells from nearby restaurant--yum!
-</li>
-<li>Walking distance to several bars and restaurants!</li>
-</ul> */
-}
-
-{
-  /* <ul>
-<li>Private Balconies and Terraces</li>
-<li>Beautiful Forest Views</li>
-<li>Carpeted Bedrooms</li>
-<li>Keypad deadbolt locks</li>
-<li>Wood Style Flooring</li>
-<li>Basic Spectrum Cable Included</li>
-<li>Exterior Storage Closet off of Balcony/Terrace</li>
-<li>Large Closets</li>
-</ul> */
-}
