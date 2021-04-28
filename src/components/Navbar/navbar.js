09b3e7752eb43ad97725f6658f466d588be97a1d
lanejm <67156874+lanejm@users.noreply.github.com>
2021-04-28 07:42:07 -0400

    hamburger nav fixed

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,40 +1,48 @@
 import React from "react";
-import {Link} from 'react-router-dom'
-// import { FaBars } from "react-icons/fa";
-// import { links } from "./data";
+import { Link } from "react-router-dom";
 import { ReactComponent as LogoSvg } from "../../assets/logo.svg";
 import "../../App.scss";
 import Auth from "../Auth/auth";
-import { Navbar } from "react-bulma-components";
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
 
 const Navigate = () => {
-  //   const [showLinks, setShowLinks] = useState(false);
-  //   const linksContainerRef = useRef(null);
-  //   const linksRef = useRef(null);
-  //   const toggleLinks = () => {
-  //     setShowLinks(!showLinks);
-  //   };
-
-  //   useEffect(() => {
-  //     const linksHeight = linksRef.current.getBoundingClientRect().height;
-  //     if (showLinks) {
-  //       linksContainerRef.current.style.height = `${linksHeight}px`;
-  //     } else {
-  //       linksContainerRef.current.style.height = "0px";
-  //     }
-  //   }, [showLinks]);
   return (
-    <Navbar class="navbar" role="navigation" aria-label="main navigation">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <LogoSvg />
-        {/* fix this so text below drop down is closer */}
-
         <a
           role="button"
           class="navbar-burger"
           aria-label="menu"
           aria-expanded="false"
-          data-target="navbarBasicExample"
+          data-target="options"
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -42,47 +50,30 @@ const Navigate = () => {
         </a>
       </div>
 
-      <div id="navbarBasicExample" class="navbar-menu">
+      <div id="options" class="navbar-menu">
         <div class="navbar-start">
-          <a href='/home' class="navbar-item">Home</a>
+          <a href="/home" class="navbar-item">
+            Home
+          </a>
 
-          <a href='/amenities' class="navbar-item">Amenities</a>
+          <a href="/amenities" class="navbar-item">
+            Amenities
+          </a>
 
-          <a href='/photos' class="navbar-item">Photos</a>
+          <a href="/photos" class="navbar-item">
+            Photos
+          </a>
 
-          <a href='/map' class="navbar-item">Map</a>
-          
+          <a href="/map" class="navbar-item">
+            Map
+          </a>
 
-          <a href='/contact' class="navbar-item">Contact</a>
+          <a href="/contact" class="navbar-item">
+            Contact
+          </a>
         </div>
-
-        {/* <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="dropdown is-active">
-              <div class="dropdown-trigger">
-                <div
-                  class="button"
-                  aria-haspopup="true"
-                  aria-controls="dropdown-menu"
-                > */}
-                  {/* <span>Resident Login</span> */}
-                  {/* <span class="icon is-small">
-                    <i class="fas fa-angle-down" aria-hidden="true"></i>
-                  </span> */}
-                {/* </div> */}
-              {/* </div> */}
-              {/* <div class="dropdown-menu" id="dropdown-menu" role="menu">
-                <div class="dropdown-content">
-                  <a href="#" class="dropdown-item">
-                    <Auth />  this dropdown needs state to toggle 
-                  </a>
-                </div>
-              </div> */}
-            {/* </div> */}
-          {/* </div> */}
-        {/* </div> */}
       </div>
-    </Navbar>
+    </nav>
   );
 };
 
