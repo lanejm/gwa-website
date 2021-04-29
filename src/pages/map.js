4f96eaf2eda92588b044dabe75d0704543db8ff4
lanejm <67156874+lanejm@users.noreply.github.com>
2021-04-29 15:15:20 -0400

    google map

--- src/pages/map.js
+++ src/pages/map.js
@@ -7,27 +7,29 @@ const mapStyles = {
 };
 
 export class Location extends Component {
-  state = {
-    showingInfoWindow: true, // Hides or shows the InfoWindow
-    activeMarker: {}, // Shows the active marker upon click
-    selectedPlace: {}, // Shows the InfoWindow to the selected place upon a marker
-  };
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true,
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stores: [{ lat: 40.0, lng: -86.0 }],
+    };
+  }
+
+  displayMarker = () => {
+    return this.state.stores.map((store, index) => {
+      return (
+        <Marker
+          key={index}
+          id={index}
+          position={{
+            lat: 39.87041873958882,
+            lng: -86.12878794390897
+          }}
+          onClick={() => console.log("You clicked me!")}
+        />
+      );
     });
-
-  onClose = (props) => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null,
-      });
-    }
   };
-
   render() {
     return (
       <Map
@@ -36,27 +38,12 @@ export class Location extends Component {
         style={mapStyles}
         initialCenter={{ lat: 39.87041873958882, lng: -86.12878794390897 }}
       >
-        <Marker
-          position={{ lat: 39.87041873958882, lng: -86.12878794390897 }}
-          name={"Glendale Woods Apartments"}
-        />
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}
-          onClose={this.onClose}
-        >
-          <div>
-            <h4>{this.state.selectedPlace.name}</h4>
-          </div>
-        </InfoWindow>
+        {this.displayMarker()}
       </Map>
     );
   }
 }
 
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=process.env.REACT_APP_API_KEY&callback=initMap"
-type="text/javascript"></script>
-
 export default GoogleApiWrapper({
-  apiKey: process.env.REACT_APP_API_KEY
+  apiKey: process.env.REACT_APP_API_KEY,
 })(Location);
